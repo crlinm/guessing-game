1b2aa7427adaa12b349ceddf5abe4b1f8e534542
Alina M <crlin.m@gmail.com>
2024-10-07 23:00:05 +0500

    feat: implementaion binary search using class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.mid = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid; 
     }
 }
 
